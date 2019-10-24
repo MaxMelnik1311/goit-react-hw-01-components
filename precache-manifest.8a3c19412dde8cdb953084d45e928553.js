bc36b83132c21b0fa29e957ba2c272582da10784
MaxMelnik1311 <maaxfresh1100@gmail.com>
2019-10-24 23:14:04 +0300

    Updates

---
+++ precache-manifest.8a3c19412dde8cdb953084d45e928553.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3dd65d1e54c1f0c2fdcddc2027461d2",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "aee142a63ca28e6217e3",
+    "url": "/goit-react-hw-01-components/static/css/main.450aa188.chunk.css"
+  },
+  {
+    "revision": "0496bd345817595c5f74",
+    "url": "/goit-react-hw-01-components/static/js/2.d045688b.chunk.js"
+  },
+  {
+    "revision": "aee142a63ca28e6217e3",
+    "url": "/goit-react-hw-01-components/static/js/main.0f0d8030.chunk.js"
+  },
+  {
+    "revision": "15b1a6c8a67382d71ce3",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.fc1c53ef.js"
+  }
+]);
